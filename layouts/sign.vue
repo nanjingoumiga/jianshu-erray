319b3e2b3df42f8c76c167b85e749e1042d25445
陈佳威 <719816921@qq.com>
2018-04-19 13:05:04 +0800

    登录注册页面完成了

--- layouts/sign.vue
+++ layouts/sign.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="sign-template">
     <nuxt/>
   </div>
 </template>
@@ -18,81 +18,93 @@ export default {
 </script>
 
 <style>
-body{
-    background: #f1f1f1;
-}
-.sign{
-    height: 100%;
-    font-size: 14px;
-    text-align: center;
-}
-.sign .logo {
-    position: absolute;
-    left: 50px;
-    top: 50px;
-}
-.sign img{
-    width: 100px;
-}
-.sign .main{
-    width:400px;
-    margin:50px auto 0;
-    padding:50px 50px 30px;
-    background:#fff;
-    border-radius: 4px;
-    box-shadow: 0 0 8px rgba(0,0,0,.1);
-    display:inline-block;
-}
-.sign .main h4 {
-    margin:0 auto 50px;
-    padding:10px;
-    color:#969696;
-    font-size:18px;
-    font-weight: 400;
-    display: block;
-}
-.sign .main h4 a,.sign .main h4 b{
-    padding: 10px;
-    color: #969696;
-}
-.sign .main h4 a.active{
-    font-weight: 700;
-    color: #ea6f5a;
-    border-bottom:2px solid #ea6f5a;
-}
-.sign .main h4 a:hover {
-    border-bottom:2px solid #ea6f5a;
-}
-.sign .main form {
-    color: #969696;
-    margin: 0 0 30px;
-}
-.sign .main form .input-prepend{
-    position: relative;
-    width: 100%;
-}
-.sign .main form .input-prepend:nth-of-type(1) input:nth-child(1){
-    border-bottom: none;
-    border-radius: 4px 4px 0 0;
-}
-.sign .main form .input-prepend input{
-    width: 100%;
-    height: 50px;
-    padding:4px 12px 4px 35px;
-    border: 1px solid #c8c8c8;
-    border-radius: 2px; 
-    background-color: rgb(248, 248, 248);
-}
-.sign .main form input.top-radius {
-    border-radius:4px 4px 0 0;
-}
-.sign .main form .input-prepend i{
-    position:absolute;
-    color:#969696;
-    top:14px;
-    left:10px;
-    font-size:20px;
-}
+    .sign {
+        height:100%;
+        font-size:14px;
+        text-align:center;
+        background-color:#f1f1f1;
+        min-height:750px;
+    }
+    .sign:before {
+        content: "";
+        display: inline-block;
+        height: 85%;
+        vertical-align: middle;
+    }
+    .sign .logo {
+        position:absolute;
+        margin-left:50px;
+        top:56px;
+    }
+    .sign img {
+        width:100px;
+    }
+    .sign .main {
+        width:400px;
+        margin:60px auto 0;
+        padding:50px 50px 30px;
+        background:#fff;
+        border-radius: 4px;
+        box-shadow: 0 0 8px rgba(0,0,0,.1);
+        display:inline-block;
+        vertical-align: middle;
+    }
+    .sign .main .title {
+        margin:0 auto 50px;
+        color:#939393;
+        font-size:18px;
+    }
+    .sign .main .title h4 {
+        font-size:18px;
+    }
+    .sign .main h4 a{
+        font-weight:400;
+        padding:10px;
+        color:#969696;
+    }
+    .sign .main h4 b {
+        font-weight:700;
+        padding:10px;
+        color:#969696;
+    }
+    .sign .main h4 a.active {
+        font-weight:700;
+        color:#ea6f5a;
+        border-bottom:2px solid #ea6f5a;
+    }
+    .sign .main h4 a:hover {
+        border-bottom:2px solid #ea6f5a;
+    }
+    .sign .main form {
+        color:#969696;
+        margin:0 0 30px;
+    }
+    .sign .main form .input-prepend {
+        position:relative;
+        width:100%;
+    }
+    .sign .main form .input-prepend input {
+        width:100%;
+        height:50px;
+        padding:4px 12px 4px 30px;
+        border:1px solid #c8c8c8;
+        background-color:rgb(248,248,248);
+        border-bottom:0;
+    }
+    .sign .main form .input-prepend:first-of-type input {
+        border-radius:2px 2px 0 0;
+    }
+    .sign .main form .input-prepend:last-of-type input {
+        border-bottom:1px solid #c8c8c8;
+        border-radius: 0 0 2px 2px;
+    }
+    .sign .main form .input-prepend i {
+        position:absolute;
+        left:10px;
+        top:15px;
+        font-size:20px;
+        color:#969696;
+    }
     .sign .main form .remember {
         float:left;
         margin:15px 0;
@@ -108,17 +120,116 @@ body{
     .sign .main form button {
         width:100%;
         padding:9px 18px;
+        margin-top:20px;
         text-align:center;
         color:#fff;
-        background:#3194D0;
         border-radius:25px;
         font-size:18px;
         border:none;
         outline: none;
+        display:block;
         clear:both;
     }
-    .sign .main form button:hover {
+    .sign .main form button.sign-in-btn {
+        background:#3194D0;
+    }
+    .sign .main form button.sign-up-btn {
+        background:rgb(66,192,46);
+    }
+    .sign .main form button.sign-in-btn:hover {
         background:#187cb7;
         cursor: pointer;
+    }
+    .sign .main form button.sign-up-btn:hover {
+        background:#3db922;
+    }
+    .sign .main form p.sign-up-msg {
+        font-size:12px;
+        margin:15px 0;
+        text-align:center;
+        line-height:20px;
+        color:#969696;
+    }
+    .sign .main form ssm {
+        position:relative;
+        line-height: 36px;
+    }
+    .sign .main form .send-ssm {
+        position:absolute;
+        right:10px;
+        top:10px;
+        width:100px;
+        height:36px;
+        line-height:36px;
+        border-radius: 20px;
+        color:#fff;
+        background:#3db922;
+        text-align:center;
+    }
+    .sign .main .more-sign p {
+        position:relative;
+        font-size:14px;
+        color:#b8b8b8;
+        margin-bottom:20px;
+    }
+    .sign .main .more-sign p:before {
+        content:'';
+        width:60px;
+        border-bottom:1px solid #b8b8b8;
+        position:absolute;
+        left:30px;
+        top:10px;
+    }
+    .sign .main .more-sign p:after {
+        content:'';
+        width:60px;
+        border-bottom:1px solid #b8b8b8;
+        position:absolute;
+        right:30px;
+        top:10px;
+    }
+    .sign .main .more-sign ul {
+        margin-bottom:10px;
+    }
+    .sign .main .more-sign ul li {
+        display:inline-block;
+        color:white;
+        margin:0 5px;
+        border-radius: 50%;
+    }
+    .sign .main .more-sign ul li a {
+        width: 50px;
+        height: 50px;
+        line-height: 50px;
+        display: block;
+    }
+    .sign .main .more-sign ul li a i {
+        font-size:24px;
+        text-align:center;
+        line-height:50px;
+    }
+    .sign .main .more-sign ul li a i.weibo {
+        color:#e05244;
+    }
+    .sign .main .more-sign ul li a i.weixin {
+        color:#00bb29;
+    }
+    .sign .main .more-sign ul li a i.qq {
+        color:#498ad5;
+    }
+    @media (max-width:768px) {
+        .sign .logo {
+            display:none;
+        }
+        .sign .main {
+            box-shadow: none;
+            margin:0 auto;
+        }
+        .sign {
+            background:#fff;
+        }
+        .sign:before {
+            height:0;
+        }
     }
 </style>
--- pages/sign-in.vue
+++ pages/sign-in.vue
@@ -1,99 +1,92 @@
 <template>
   <div class="sign">
-    <!-- logo -->
-    <div class="logo">
-      <nuxt-link to="/">
-        <img src="~/assets/img/logo.png" alt="">
-      </nuxt-link>
-    </div>
-    <!-- 登录表单 -->
-    <div class="main">
-      <h4>
-        <nuxt-link class="active" to="/sign-in">
-          登录
-        </nuxt-link>
-        <b>·</b>
-        <nuxt-link to="/sign-up">
-          注册
-        </nuxt-link>
-      </h4>
-      <!-- form表单 -->
-      <div class="js-form">
-        <form method="post">
-          <div class="input-prepend">
-            <input type="text" placeholder="手机号或邮箱">
-            <i class="fa fa-user"></i>
-          </div>
-          <div class="input-prepend">
-            <input type="text" placeholder="密码">
-            <i class="fa fa-lock"></i>
-          </div>
-          <div class="remember">
-            <input type="checkbox" checked>&nbsp;
-            <span>记住我</span>
-          </div>
-          <div class="help">
-            <nuxt-link to="/help">
-              登录遇到问题?
+        <!--logo-->
+        <div class="logo">
+            <nuxt-link to="/">
+                <img src="../assets/img/logo.png">
             </nuxt-link>
-          </div>
-          <button>登录</button>
-        </form>
-      </div>
-      <!-- 第三方的登录 -->
-      <div class="more-sign">
-        <p>社交账号登录</p>
-        <ul>
-          <li class="weibo">
-            <a href="#">
-              <i class="fa fa-weibo"></i>
-            </a>
-          </li>
-          <li class="weixin">
-            <a href="#">
-              <i class="fa fa-weixin"></i>
-            </a>
-          </li>
-          <li class="qq">
-            <a href="#">
-              <i class="fa fa-qq"></i>
-            </a>
-          </li>
-          <li class="other">
-            <a href="#">
-              其他
-            </a>
-          </li>
-        </ul>
-      </div>
+        </div>
+        <!--主体部分-->
+        <div class="main">
+            <!--title-->
+            <div class="title">
+                <h4>
+                    <nuxt-link class="active" to="/sign-in">
+                        登录
+                    </nuxt-link>
+                    <b>·</b>
+                    <nuxt-link class to="/sign-up">
+                        注册
+                    </nuxt-link>
+                </h4>
+            </div>
+            <!--form表单-->
+            <div class="js-form">
+                <form method="post">
+                    <div class="input-prepend">
+                        <input type="text"  placeholder="手机号或邮箱">
+                        <i class="fa fa-user"></i>
+                    </div>
+                    <div class="input-prepend">
+                        <input type="text"  placeholder="密码">
+                        <i class="fa fa-lock"></i>
+                    </div>
+                    <div class="remember">
+                        <input type="checkbox">
+                        <span>记住我</span>
+                    </div>
+                    <div class="help">
+                        <nuxt-link to="/">
+                            登录遇到问题?
+                        </nuxt-link>
+                    </div>
+                    <button class="sign-in-btn">登录</button>
+                </form>
+            </div>
+            <!--第三方的登录-->
+            <div class="more-sign">
+                <p>社交账号登录</p>
+                <ul>
+                    <li>
+                        <nuxt-link to="/">
+                            <i class="weixin fa fa-weixin"></i>
+                        </nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/">
+                            <i class="weibo fa fa-weibo"></i>
+                        </nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/">
+                            <i class="qq fa fa-qq"></i>
+                        </nuxt-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import axios from "~/plugins/axios";
-
-export default {
+    export default {
   // 修改当前页面的默认模板为sign.vue
   // 如果不写的话,默认是default.vue
-  layout: "sign",
+        layout: "sign",
   // 设置页面的标题以及描述,关键字等元信息
-  head: {
-    title: "登录 - 简书",
-    meta: [
-      { charset: "utf-8" },
-      { name: "keywords", content: "登录页面" },
-      { name: "description", content: "登录简书" },
-      { name: "author", content: "陈佳威" }
-    ]
-  },
-  data() {
-    return {
-      name: "登录"
-    };
-  }
-};
-</script>
+        head: {
+            title: "登录 - 简书",
+            meta: [
+                { charset: "utf-8" },
+                { name: "keywords", content: "登录页面" },
+                { name: "description", content: "登录简书" },
+                { name: "author", content: "陈佳威" }
+            ]
+        },
+        data() {
+            return {
 
-<style scoped>
-</style>
+            };
+        }
+    };
+</script>
