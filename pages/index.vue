c46d80bb834c034dc06fa07447dc051fd5e119e3
陈佳威 <719816921@qq.com>
2018-04-23 11:05:59 +0800

    第一次创建分支并使用

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div style="height:100%">
+       <my-header></my-header>
+    </div>
+</template>
+<script>
+    import myHeader from  '../components/myHeader'
+    export default {
+        head:{
+            title:'简书-创作你的创作',
+            meta:[
+                {charset:'utf-8'},
+                {name:'keywords',content:'简书首页'},
+                {name:'description',content:'简书首页'},
+                {name:'author',content:'陈佳威'}
+            ]
+        },
+        components:{
+            myHeader
+        },
+        data () {
+            return {
+                name:'首页'
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
